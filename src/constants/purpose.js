368690063635471eb5c44826de943518fe10e02d
krystek77 <krystian.wrona77@gmail.com>
2021-10-10 22:52:26 +0200

    update purpose data

--- src/constants/purpose.js
+++ src/constants/purpose.js
@@ -5,4 +5,5 @@ export default [
     {id:4,purpose:"prasowanie tkanin płaskich"},
     {id:5,purpose:"prasowanie fasonowe"},
     {id:6,purpose:"pralnia samoobsługowa"},
+    {id:7,purpose:"farbowanie"},
 ]
