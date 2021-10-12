c35d5d0521e75750d2c8863879c152980eae9819
krystek77 <krystian.wrona77@gmail.com>
2021-10-12 13:42:05 +0200

    create technical data component

--- src/components/TechnicalData/TechnicalData.js
+++ src/components/TechnicalData/TechnicalData.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './TechnicalData.css';
 
-function TechnicalData() {
-  return (
-    <div>table</div>
-  );
+function TechnicalData({ data }) {
+  function generateTable() {
+    const header = data[0];
+    const amount = data[0].values.length;
+    const body = data.slice(1);
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>{header.name}</th>
+            <th>{header.unit}</th>
+            {header.values.map((item) => {
+              return <th key={item}>{item}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {body.map((row)=>{
+            const values = row.values;
+            const length = values.length;
+
+            return (
+              <tr key={row.id}>
+                <td>{row.name}</td>
+                <td>{row.unit}</td>
+                {values.map((item,index)=>{
+                  if (length > 1) {
+                    return <td key={index}>{item}</td>;
+                  }
+                  return <td colSpan={amount} key={item}>{item.split(',').map((s,index)=>{
+                    return <div key={index}>{s}</div>
+                  })}</td>;
+                })}
+              </tr>
+            );
+          })}
+          
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>{header.name}</th>
+            <th>{header.unit}</th>
+            {header.values.map((item) => {
+              return <th key={item}>{item}</th>;
+            })}
+          </tr>
+          <tr>
+            <th><b>*</b></th>
+            <th>-</th>
+            <th colSpan={amount}>= standard</th>
+          </tr>
+           <tr>
+            <th><b>N/A</b></th>
+            <th>-</th>
+            <th colSpan={amount}>= nie dostępne</th>
+          </tr>
+        </tfoot>
+      </table>
+    );
+  }
+
+  return data.length !== 0 && generateTable();
 }
+TechnicalData.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      values: PropTypes.array,
+    })
+  ),
+};
 export default TechnicalData;
