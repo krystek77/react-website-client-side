e5e7612822daa6ebffba492f9940ab6fe59d5206
krystek77 <krystian.wrona77@gmail.com>
2021-10-15 10:27:52 +0200

    move router component to index.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './index.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ProductDetails from './components/ProductDetails/ProductDetails';
 
 import Layout from './components/Layout/Layout';
 
 const App = () => {
   return (
-    <Router>
       <Layout>
         <Switch>
           <Route path="/produkty/:name" render={(props) => <ProductDetails {...props} />} />
           <Route path="/" render={() => <h1>Home Page</h1>} />
         </Switch>
       </Layout>
-    </Router>
   );
 };
 export default App;
