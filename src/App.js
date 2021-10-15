7904294b96b9c56aac9d34c44e768e861b564bbb
krystek77 <krystian.wrona77@gmail.com>
2021-10-15 11:02:01 +0200

    add another routes

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ const App = () => {
   return (
       <Layout>
         <Switch>
+          <Route path="/o-nas" render={() => <h1 style={{flexGrow:1,display:'flex',justifyContent:'center',alignItems:'center'}}>strona o nas</h1>} />
+          <Route path="/klienci" render={() => <h1 style={{flexGrow:1,display:'flex',justifyContent:'center',alignItems:'center'}}>strona klienci</h1>} />
           <Route path="/produkty/:name" render={(props) => <ProductDetails {...props} />} />
-          <Route path="/" render={() => <h1>Home Page</h1>} />
+          <Route path="/technologie" render={() => <h1 style={{flexGrow:1,display:'flex',justifyContent:'center',alignItems:'center'}}>strona technologie</h1>} />
+          <Route path="/akcesoria" render={() => <h1 style={{flexGrow:1,display:'flex',justifyContent:'center',alignItems:'center'}}>strona akcesoria</h1>} />
+          <Route path="/wiadomosci" render={() => <h1 style={{flexGrow:1,display:'flex',justifyContent:'center',alignItems:'center'}}>strona widaomosci</h1>} />
+          <Route path="/kontakt" render={() => <h1 style={{flexGrow:1,display:'flex',justifyContent:'center',alignItems:'center'}}>strona kontakt</h1>} />
+          <Route path="/" render={() => <h1 style={{flexGrow:1,display:'flex',justifyContent:'center',alignItems:'center'}}>strona startowa</h1>} />
         </Switch>
       </Layout>
   );
