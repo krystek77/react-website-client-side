ccdfb2450e33e3186a56d0ad9038b4f7ba4869dd
krystek77 <krystian.wrona77@gmail.com>
2021-10-20 10:45:59 +0200

    add contact button to toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,7 +1,12 @@
 import React from "react";
 import './Toolbar.css';
+import {ContactMail} from '@mui/icons-material';
 
 function Toolbar(){
-    return <div className="toolbar">Toolbar</div>
+    return <div className="toolbar">
+        <div className="toolbar__menu">
+            <button type="button" className="toolbar__mail"><ContactMail/></button>
+        </div>
+    </div>
 }
 export default Toolbar;
