82acebd18b0aa7b4acd2c357f0fadbc29ea7b634
krystek77 <krystian.wrona77@gmail.com>
2021-11-02 11:05:10 +0100

    Use hero component on about us page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,23 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import {makeStyles} from '@mui/styles'
 
-function About(){
-    return <div className="page"><span>About Page</span></div>
+const useStyles = makeStyles((theme)=>({
+
+}))
+
+import Hero from '../Hero/Hero';
+
+
+function About() {
+const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Hero blend="grey" bgImage="" title="Dlaczego Pralma ..." subtitle="" />
+      <main className={[`${classes.aboutPage}`, 'page'].join(' ')}>
+        <span>About us Page</span>
+      </main>
+    </React.Fragment>
+  );
 }
 export default About;
