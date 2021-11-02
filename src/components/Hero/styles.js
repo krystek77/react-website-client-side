4acb38d11d6610b17f875f97eec53b0d84af3a12
krystek77 <krystian.wrona77@gmail.com>
2021-11-02 10:53:32 +0100

    Create hero component

---
+++ src/components/Hero/styles.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import {makeStyles} from '@mui/styles';
+import { lineHeight } from '@mui/system';
+
+export default makeStyles((theme)=>({
+    hero:{
+        backgroundColor:(props)=>props.blend ? props.blend:theme.palette.secondary.light,
+        backgroundImage:(props)=>props.bgImage ? `url(../assets/images/hero/${props.bgImage}.webp)`:"transparent",
+        backgroundBlendMode:"difference",
+        backgroundSize:"cover",
+        backgroundPosition:"center center",
+        [theme.breakpoints.up("md")]:{
+            backgroundPosition:"center bottom"
+        },
+        [theme.breakpoints.up("md")]:{
+            backgroundPosition:"center"
+        },
+        flexShrink:0,
+        height:400,
+        width:"100%"
+    },
+    ['hero__container']:{
+        height:"100%",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        [theme.breakpoints.up('md')]:{
+            justifyContent:'flex-start',
+        }
+    },
+    ['hero__content']:{
+        backgroundColor:'rgba(0,0,0,0.65)',
+        maxWidth:400,
+        padding:32,
+        borderRadius:4
+    },
+    ['hero__title']:{
+        color:theme.palette.secondary.contrastText,
+        marginBottom:0,
+        lineHeight:1.15
+    },
+    ['hero__subtitle']:{
+        color:theme.palette.secondary.contrastText,
+        marginBottom:0
+    }
+}));
