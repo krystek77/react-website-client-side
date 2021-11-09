56ea843be364d25909692e94222a9cf0a5a3bbc9
krystek77 <krystian.wrona77@gmail.com>
2021-11-09 10:18:27 +0100

    Set post form under posts on small screens

--- src/pages/News.js
+++ src/pages/News.js
@@ -26,30 +26,32 @@ function News() {
   return (
     <div className={`${classes['page']} ${classes['page--news']}`}>
       <Hero title="Bądź na bieżąco" subtitle="wszystko co warto wiedzieć w pralnictwie" blendColor="green" />
-      {/** form post */}
-      <Container className={classes['page__section']} component="section" maxWidth="false">
-        <Container className={classes['page__header']} component="header" maxWidth="false">
-          <Typography className={`${classes['page__title']} ${classes['page__title--center']}`} variant="h2" component="h2">
-            {currentPostID ? "Formularz edytowania wiadomości" : "Formularz dodawania wiadomości"}
-          </Typography>
+      <div className={classes['page-wrapper']}>
+        {/** form post */}
+        <Container className={classes['page__section']} component="section" maxWidth="false">
+          <Container className={classes['page__header']} component="header" maxWidth="false">
+            <Typography className={`${classes['page__title']} ${classes['page__title--center']}`} variant="h2" component="h2">
+              {currentPostID ? "Formularz edytowania wiadomości" : "Formularz dodawania wiadomości"}
+            </Typography>
+          </Container>
+
+          <Container maxWidth="md">
+            <PostForm currentPostID={currentPostID} setCurrentPostID={setCurrentPostID} />
+          </Container>
         </Container>
 
-        <Container maxWidth="md">
-          <PostForm currentPostID={currentPostID} setCurrentPostID={setCurrentPostID} />
+        {/** posts */}
+
+        <Container className={classes['page__section']} component="main" maxWidth="false">
+          <Container className={classes['page__header']} component="header" maxWidth="false">
+            <Typography className={`${classes['page__title']}`} variant="h2" component="h2">
+              Wiadomości
+            </Typography>
+          </Container>
+
+          <Posts setCurrentPostID={setCurrentPostID}/>
         </Container>
-      </Container>
-
-      {/** posts */}
-
-      <Container className={classes['page__section']} component="main" maxWidth="false">
-        <Container className={classes['page__header']} component="header" maxWidth="false">
-          <Typography className={`${classes['page__title']}`} variant="h2" component="h2">
-            Wiadomości
-          </Typography>
-        </Container>
-
-        <Posts setCurrentPostID={setCurrentPostID}/>
-      </Container>
+      </div>
     </div>
   );
 }
