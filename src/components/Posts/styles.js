7623182baa45c802e3f77d5dec74646de084a298
krystek77 <krystian.wrona77@gmail.com>
2021-11-27 18:49:10 +0100

    Remove unneccessary styles for posts component

--- src/components/Posts/styles.js
+++ src/components/Posts/styles.js
@@ -1,5 +1,5 @@
 import { makeStyles } from '@mui/styles';
-export default makeStyles((theme) => ({
+export default makeStyles(() => ({
   posts: {
     marginBottom: 48,
   },
@@ -8,30 +8,4 @@ export default makeStyles((theme) => ({
     paddingRight: 8,
     paddingBottom: 16,
   },
-  //Spinner
-  loading: {
-    border: `2px solid ${theme.palette.secondary.light}`,
-    borderRadius:6,
-    width: 360,
-    margin: '0 auto',
-    padding: 24,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 48,
-  },
-  loadingText: {
-    marginBottom: 24,
-    color:theme.palette.secondary.dark
-  },
-  loadingImage: {
-      color:theme.palette.secondary.dark
-  },
-  //Feedback
-  feedBack:{
-    marginBottom: 48,
-    color:theme.palette.secondary.dark,
-    textAlign:"center"
-  }
 }));
