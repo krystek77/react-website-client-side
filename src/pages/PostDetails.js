54cd5cb065da81b8190f5e4ece76ae9ef7f942ab
krystek77 <krystian.wrona77@gmail.com>
2021-12-06 13:45:42 +0100

    Add like and delete post on post details page too

--- src/pages/PostDetails.js
+++ src/pages/PostDetails.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { Container, Typography, Chip, Divider, Button } from '@mui/material';
-import { KeyboardReturn } from '@mui/icons-material';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Container, Typography, Chip, Divider, Button, IconButton } from '@mui/material';
+import { KeyboardReturn, Favorite, Delete } from '@mui/icons-material';
 import useStyles from '../styles/PostDetails';
 import moment from 'moment';
 import 'moment/locale/pl';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { getPostById } from '../actions/posts';
+import { getPostById, deletePost, likePost } from '../actions/posts';
 
 moment.locale('pl');
 
@@ -16,9 +16,14 @@ import Hero from '../components/Hero/Hero';
 import Loading from '../components/Loading/Loading';
 
 function PostDetails() {
-  const { isLoading, post } = useSelector((state) => state.posts);
+  const { isLoading, post } = useSelector((state) => {
+    console.log('Invoke useSelector from PostDetails');
+    return state.posts;
+  });
+  const userProfile = JSON.parse(localStorage.getItem('userProfile'));
   const { id } = useParams();
   const dispatch = useDispatch();
+  const history = useNavigate();
 
   const classes = useStyles();
 
@@ -31,6 +36,13 @@ function PostDetails() {
   }, [dispatch, id]);
 
   if (isLoading) return <Loading mt={48} message="Pobieranie szczegółów ..." />;
+  console.log('PostDetails RELOAD');
+  console.log(post);
+
+  const handleDeletePost = () => {
+    dispatch(deletePost(post._id));
+    history('/wiadomosci');
+  };
 
   return post ? (
     <React.Fragment>
@@ -47,11 +59,18 @@ function PostDetails() {
           <Chip className={classes.postTag} key={tag} label={tag} variant="filled" size="small" />
         ))}
         <Typography className={classes.postAuthor} variant="caption" component="p">
-          autor:{' '}
+          autor:
           <Typography className={classes.postAuthorName} component="span" variant="caption">
             {post.author.firstName}
           </Typography>
         </Typography>
+        <Divider className={classes.postDivider} light />
+        <Button onClick={() => dispatch(likePost(post._id))} disabled={!userProfile || userProfile?.user._id === post.author._id} className={`${classes['post__btn']} ${classes['post__btn--favorite']}`} startIcon={<Favorite />}>
+          {post.likes.length}
+        </Button>
+        <IconButton disabled={!userProfile || !(userProfile?.user._id === post.author._id)} onClick={handleDeletePost} color="secondary" className={`${classes['post__btn']} ${classes['post__btn--delete']}`}>
+          <Delete />
+        </IconButton>
         <Divider className={classes.postDivider} light />
         <Button className={classes.postReturnButton} component={Link} to="/wiadomosci" variant="contained" size="small" startIcon={<KeyboardReturn />}>
           Wszystkie wiadomości
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,7 @@
 import ActionTypes from '../constants/actionTypes';
-const initialState = { isLoading: true, posts: [], post: null,numberOfPages:0 };
+const initialState = { isLoading: true, posts: [], post: null, numberOfPages: 0 };
 export default function reducer(state = initialState, action) {
-  // console.log('POSTS REDUCER', action.payload);
+  console.log('POSTS REDUCER', action.payload);
   if (state === undefined || state === null) return initialState;
   switch (action.type) {
     case ActionTypes.START_LOADING_POSTS:
@@ -11,7 +11,7 @@ export default function reducer(state = initialState, action) {
     case ActionTypes.GET_POST_BY_ID:
       return { ...state, post: action.payload };
     case ActionTypes.GET_POSTS:
-      return { ...state, posts: action.payload.posts,numberOfPages:action.payload.numberOfPages };
+      return { ...state, posts: action.payload.posts, numberOfPages: action.payload.numberOfPages };
     case ActionTypes.END_LOADING_POSTS:
       return { ...state, isLoading: false };
     case ActionTypes.CREATE_POST:
@@ -19,9 +19,9 @@ export default function reducer(state = initialState, action) {
     case ActionTypes.UPDATE_POST:
       return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
     case ActionTypes.LIKE_POST:
-      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
+      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)), post: action.payload };
     case ActionTypes.DELETE_POST:
-      return { ...state, posts: state.posts.filter((post) => post._id !== action.payload) };
+      return { ...state, posts: state.posts.filter((post) => post._id !== action.payload), post: null };
     default:
       return state;
   }
