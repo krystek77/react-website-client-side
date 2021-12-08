f613482928d7467f7e856ecd3defa9e80812f77b
krystek77 <krystian.wrona77@gmail.com>
2021-12-08 10:46:58 +0100

    Set width 100% for section title component

--- src/components/SectionTitle/styles.js
+++ src/components/SectionTitle/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@mui/styles';
 
 export default makeStyles((theme) => ({
   sectionTitle: {
+    width: '100%',
     fontSize: 32,
     fontWeight: 800,
     textTransform: 'uppercase',
