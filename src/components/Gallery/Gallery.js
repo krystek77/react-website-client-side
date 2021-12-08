042256ffaa986436286b20435f75314170079949
krystek77 <krystian.wrona77@gmail.com>
2021-12-08 10:33:56 +0100

    Finish a sample static gallery structure

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,27 +1,71 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Container, ImageList, ImageListItem } from '@mui/material';
+import { Container, Typography, ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
 import useStyles from './styles';
 
 function Gallery() {
   const classes = useStyles();
   return (
-    <Container maxWidth="false" component="section" className={classes.imageListWrapper}>
-      <ImageList className={classes.imageList} rowHeight={200} gap={5}>
-        <ImageListItem className={classes.imageListItem}>
-          <img src="1.webp" alt="1" />
+    <Container maxWidth="false" component="section" className={classes.imageListSection}>
+      <Typography className={classes.imageListSectionTitle} component="h2" variant="h2">
+        Wybrane realizacje pralni
+      </Typography>
+      <ImageList className={classes.imageList} rowHeight={150} gap={5} cols={6}>
+        <ImageListItem className={classes.imageListItem} cols={2} rows={1}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=300&h=150&fit=crop&auto=format" loading="lazy" alt="1" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
         </ImageListItem>
-        <ImageListItem className={classes.imageListItem}>
-          <img src="2.webp" alt="2" />
+        <ImageListItem className={classes.imageListItem} cols={2} rows={2}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=300&h=300&fit=crop&auto=format" loading="lazy" alt="2" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Hotel***, Słupsk" position="top" />
         </ImageListItem>
-        <ImageListItem className={classes.imageListItem}>
-          <img src="3.webp" alt="3" />
+        <ImageListItem className={classes.imageListItem} cols={1} rows={1}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1589118949245-7d38baf380d6?w=150&h=150&fit=crop&auto=format" loading="lazy" alt="3" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Klub sportowy, LECH" position="top" />
         </ImageListItem>
-        <ImageListItem className={classes.imageListItem}>
-          <img src="4.webp" alt="4" />
+        <ImageListItem className={classes.imageListItem} cols={1} rows={1}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1471357674240-e1a485acb3e1?w=150&h=150&fit=crop&auto=format" loading="lazy" alt="4" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
         </ImageListItem>
-        <ImageListItem className={classes.imageListItem}>
-          <img src="5.webp" alt="5" />
+        <ImageListItem className={classes.imageListItem} cols={2} rows={2}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1567306301408-9b74779a11af?w=300&h=300&fit=crop&auto=format" loading="lazy" alt="5" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="SPZOZ w Lipsku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={1} rows={1}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1597645587822-e99fa5d45d25?w=150&h=150&fit=crop&auto=format" alt="6" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={1} rows={2}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1518756131217-31eb79b20e8f?w=150&h=300&fit=crop&auto=format" alt="7" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={1} rows={1}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=150&h=150&fit=crop&auto=format" alt="8" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={2} rows={1}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=300&h=150&fit=crop&auto=format" alt="9" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={1} rows={1}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1588436706487-9d55d73a39e3?w=150&h=150&fit=crop&auto=format" alt="10" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={2} rows={2}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1549388604-817d15aa0110?w=300&h=300&fit=crop&auto=format" alt="11" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={1} rows={2}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1563298723-dcfebaa392e3?w=150&h=300&fit=crop&auto=format" alt="12" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={2} rows={2}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1549388604-817d15aa0110?w=300&h=300&fit=crop&auto=format" alt="13" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
+        </ImageListItem>
+        <ImageListItem className={classes.imageListItem} cols={1} rows={1}>
+          <img className={classes.imageListItemImage} src="https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=150&h=150&fit=crop&auto=format" alt="14" />
+          <ImageListItemBar className={classes.imageListItemCaption} title="Dom Pomocy Społecznej w Rybniku" position="top" />
         </ImageListItem>
       </ImageList>
     </Container>
--- src/components/Gallery/styles.js
+++ src/components/Gallery/styles.js
@@ -2,14 +2,34 @@
 import { makeStyles } from '@mui/styles';
 
 export default makeStyles((theme) => ({
-  imageListWrapper: {
+  imageListSection: {
     paddingLeft: 0,
     paddingRight: 0,
+    paddingTop: 48,
+    paddingBottom: 48,
+  },
+  imageListSectionTitle: {
+    fontSize: 32,
+    fontWeight: 800,
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    paddingBottom: 48,
+    paddingLeft: 24,
+    paddingRight: 24,
+    color: '#1E1E1E',
   },
   imageList: {
     marginBottom: 0,
+    overflow: 'hidden',
   },
   imageListItem: {
-    border: '1px solid green',
+    // border: '1px solid green',
+    '& $imageListItemImage': {
+      width: 'auto',
+    },
+  },
+  imageListItemImage: {},
+  imageListItemCaption: {
+    background: 'linear-gradient(to bottom,rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
 }));
