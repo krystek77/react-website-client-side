e30df9a8ea6972f778b9105795ecfbb0312e745f
krystek77 <krystian.wrona77@gmail.com>
2021-12-15 09:43:11 +0100

    Create customers list section

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Card, CardContent, CardMedia, Typography, Button } from '@mui/material';
+import { Container, Card, CardContent, Box, CardMedia, Typography, Button } from '@mui/material';
 import SectionTitle from '../components/SectionTitle/SectionTitle';
 import Hero from '../components/Hero/Hero';
 
@@ -13,39 +13,40 @@ function Customers() {
       <Hero title="Sektory, którym oferujemy uslugi" subtitle="Różne potrzeby, różne pralnie..., jeden dostawca - Pralma" />
       <Container component="section" className={`${classes.page} ${classes.pageCustomers}`} maxWidth="false">
         <SectionTitle title="Nasi odbiorcy" />
+
         <Container className={classes.customersList}>
           <Card className={classes.customersListItem}>
-            <CardContent className={classes.customerListItemContent}>
-              <Typography className={classes.customerListItemTitle} component="h3" variant="h3">
+            <CardContent className={classes.customersListItemContent}>
+              <Typography className={classes.customersListItemTitle} component="h3" variant="h4">
                 Kampingi i centra rekracji
               </Typography>
-              <Typography className={classes.customerListItemSubtitle} component="p" variant="body1">
+              <Typography className={classes.customersListItemSubtitle} component="p" variant="body1">
                 Oferta dla miłośników świeżego powietrza oraz dodatkowe usługi przy niskich kosztach inwestycyjnych z Twojej strony
               </Typography>
-              <Button className={classes.customerListItemActionButton} component={Link} to="/" size="large" variant="contained" color="primary">
+              <Button className={classes.customersListItemActionButton} component={Link} to="/" size="large" variant="outlined" color="primary">
                 Dowiedz się więcej
               </Button>
             </CardContent>
-            <div className={classes.customListItemImageWrapper}>
-              <CardMedia component="img" image="../assets/images/customers/campsite.jpg" alt="campsites" />
-            </div>
+            <Box className={classes.customersListItemImageWrapper}>
+              <CardMedia className={classes.customersListItemImage} component="img" image="../assets/images/customers/campsite.jpg" alt="campsites" />
+            </Box>
           </Card>
 
           <Card className={classes.customersListItem}>
-            <CardContent className={classes.customerListItemContent}>
-              <Typography className={classes.customerListItemTitle} component="h3" variant="h3">
+            <CardContent className={classes.customersListItemContent}>
+              <Typography className={classes.customersListItemTitle} component="h3" variant="h4">
                 Zdrowie i higiena
               </Typography>
-              <Typography className={classes.customerListItemSubtitle} component="p" variant="body1">
+              <Typography className={classes.customersListItemSubtitle} component="p" variant="body1">
                 Zalety zintegrowanej pralni w szpitalu, przychodni, laboratorium, pomieszczeniu czystym lub przychodni weterynaryjnej.
               </Typography>
-              <Button className={classes.customerListItemActionButton} component={Link} to="/" size="large" variant="contained" color="primary">
+              <Button className={classes.customersListItemActionButton} component={Link} to="/" size="large" variant="outlined" color="primary">
                 Dowiedz się więcej
               </Button>
             </CardContent>
-            <div className={classes.customListItemImageWrapper}>
+            <Box className={classes.customersListItemImageWrapper}>
               <CardMedia className={classes.customersListItemImage} component="img" image="../assets/images/customers/healthandhygiene.jpg" alt="health and hygiene" />
-            </div>
+            </Box>
           </Card>
         </Container>
       </Container>
--- src/styles/customers.js
+++ src/styles/customers.js
@@ -4,8 +4,8 @@ import { makeStyles } from '@mui/styles';
 export default makeStyles((theme) => ({
   pageCustomers: {
     paddingTop: 48,
-    // backgroundImage:'linear-gradient(to bottom,rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.35) 70%, rgba(0,0,0,0.35) 100%)'
   },
+
   //customer card
   customersList: {
     paddingBottom: 24,
@@ -15,28 +15,60 @@ export default makeStyles((theme) => ({
     marginBottom: 24,
     boxShadow: 'none',
     borderRadius: 0,
-    '&:nth-child(2n)': {
-      flexDirection: 'row-reverse',
+    flexDirection: 'column',
+    [theme.breakpoints.up('sm')]: {
+      flexDirection: 'row',
+      flexWrap: 'nowrap',
+      '&:nth-child(even)': {
+        flexDirection: 'row-reverse',
+      },
     },
   },
-  customerListItemContent: {
+  customersListItemContent: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    width:"100%",
-    padding:24,
+    width: '100%',
+    marginBottom: 8,
+
+    [theme.breakpoints.up('sm')]: {
+      padding: 24,
+      marginBottom: 0,
+    },
   },
-  customerListItemTitle: {},
-  customerListItemSubtitle: {},
-  customerListItemActionButton: {},
-  customListItemImageWrapper:{
-      width:"100%",
-      padding:8,
+  customersListItemTitle: {
+    marginBottom: 24,
+    color: theme.palette.secondary.main,
+  },
+  customersListItemSubtitle: {
+    marginBottom: 24,
+    color: theme.palette.secondary.main,
+  },
+  customersListItemActionButton: {
+    width: 'max-content',
+    margin: '0 auto',
+  },
+  customersListItemImageWrapper: {
+    width: '100%',
+    padding: 1,
+    border: `10px solid ${theme.palette.primary.main}`,
+    borderRadius: 4,
+    backgroundColor: theme.palette.primary.dark,
+    [theme.breakpoints.up('sm')]: {
+      margin: 24,
+    },
   },
   customersListItemImage: {
     display: 'block',
-    width:"100%",
-    height:"100%",
-    objectFit:"cover",
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    objectPosition: '50% 50%',
+  },
+  divider: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#1E1E1E',
+    background: 'linear-gradient(left right,rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.35) 70%, rgba(0,0,0,0) 100%)',
   },
 }));
